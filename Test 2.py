a92df6d2c2742fcdc3a4a34267dba478a90e3f27
Guz Vinueza <gvinueza@gobetterfly.com>
2022-07-25 19:22:12 -0500

    Prueba de más archivos

---
+++ Test 2.py
@@ -0,0 +1,9 @@
+# Test 2
+t2 = 2500
+t1 = 100
+t3 = t1 * 10 / t2
+print ('branch code example=',t3)
+
+t4 = 100
+print ('t4=',t4)
+print ('done')
---
+++ test.py
@@ -0,0 +1,14 @@
+from regex import B
+
+
+a = 10
+b = 20
+c = a * b
+d = c + 10
+e = 200 * c
+print('result=',c)
+
+print('final result=',d)
+
+print ('product=',e)
+
